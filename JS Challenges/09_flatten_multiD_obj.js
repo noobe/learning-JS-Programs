e666cd4104ce866e59800673e3b1b9bed274483e
Sarath <sarath.regedit@gmail.com>
2022-10-05 11:32:05 +0530

    Flatten Obj

---
+++ JS Challenges/09_flatten_multiD_obj.js
@@ -0,0 +1,25 @@
+let test = {a:1, b:2, c:{d:3, e: {f:4, g:5, h: {i: 6}}}}
+
+function objFlattener (obj) {
+    let res = {};
+    const prefixer = (prefixString, obj) => {
+        return Object.keys(obj).reduce((acc, key) => {
+            acc[`${prefixString}.${key}`] = obj[key];
+            return acc;
+        }, {});
+    };
+    Object.keys(obj).forEach(key => {
+        if(typeof obj[key] !== 'object') {
+            res[key] = obj[key];
+        }
+        else {
+            res = { 
+                ...res,
+                ...prefixer(key, objFlattener(obj[key]))
+            }
+        }
+    })
+    return res;
+}
+
+objFlattener(test);
